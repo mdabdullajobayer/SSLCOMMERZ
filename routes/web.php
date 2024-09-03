442a293c6403e3e7067d36d1e124a92aa7029894
mdabdullajobayer <mdabdullajovayer@gmail.com>
2024-09-03 22:02:44 +0600

    SSLCommerz Integration by using sslcommerz library

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/example1', [PaymentController::class, 'exampleEasyCheckout']);
+Route::get('/example2', [PaymentController::class, 'exampleHostedCheckout']);
+
+Route::post('/pay', [PaymentController::class, 'index']);
+Route::post('/pay-via-ajax', [PaymentController::class, 'payViaAjax']);
+
+Route::post('/success', [PaymentController::class, 'success']);
+Route::post('/fail', [PaymentController::class, 'fail']);
+Route::post('/cancel', [PaymentController::class, 'cancel']);
+
+Route::post('/ipn', [PaymentController::class, 'ipn']);
